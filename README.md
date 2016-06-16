bb735b43bdc33a59caba896019dc65d94dd6c5b7
adarshdch <adarshdch@gmail.com>
2016-06-16 18:29:20 +0530

    Updated comments

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # webpack-dev-server-demo
 How to use demo for webpack-dev-server
+
+
+**References:**
+*
+https://webpack.github.io/docs/webpack-dev-server.html*
--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
 alert('I am alerting from main.js file which is entry point!');
 alert(' Wohoo, I get udpated when there is any change in js files!!!');
-alert('Hot module replacement');
+alert('Hot module replacement!!!');
