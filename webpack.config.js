8bd6b3471b8687e9cca59f2f10a5bbd8ca0dd045
adarshdch <adarshdch@gmail.com>
2016-06-16 18:09:32 +0530

    Support hot module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require("path");
 module.exports = {
-  entry: ['./src/main.js'],
+  entry: {
+    app: ['./src/main.js']
+  },
   output: {
     path: path.resolve(__dirname, "public"),
     publicPath: "",
